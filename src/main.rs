69fcf8901d7f2f5d896c52788ae86a4a2f7d4222
wispl <wispl.8qbkk@slmail.me>
2024-08-07 01:34:02 -0400

    refactor: shift code around to prepare for rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     env, fs,
     io::{self, Write},
-    string::ToString,
     time::Duration,
 };
 
@@ -10,34 +9,47 @@ use anyhow::{Context, Result};
 use serde_json::Value;
 
 use crossterm::{
-    cursor,
+    cursor::{self, MoveTo, MoveToColumn, MoveToNextLine},
     event::{poll, read, Event, KeyCode},
     execute,
     queue,
-    style::{Color, Print, ResetColor, SetForegroundColor},
+    style::{Print, ResetColor, Color, SetForegroundColor},
     terminal,
     // ExecutableCommand,
     QueueableCommand,
 };
 
-#[derive(Default)]
-struct RenderData {
-    level: usize,
+struct RenderData<'a> {
+    value: &'a Value,
+    prev_node: Option<&'a Value>,
+    curr_node: &'a Value,
     index: usize,
-    current: String,
     old_indicies: Vec<usize>,
     path: Vec<String>,
 }
 
-impl RenderData {
-    fn index(&self) -> usize {
-        self.index
+impl<'a> RenderData<'a> {
+    fn new(value: &'a Value) -> RenderData {
+        RenderData {
+            value,
+            prev_node: None,
+            curr_node: value,
+            index: 0,
+            old_indicies: Vec::new(),
+            path: Vec::new(),
+        }
     }
 
-    // TODO: implement
-    fn indent(&self) -> u16 {
-        0
-        // (self.level * 4).try_into().unwrap()
+    fn prev_node(&self) -> Option<&'a Value> {
+        self.prev_node
+    }
+
+    fn curr_node(&self) -> &'a Value {
+        self.curr_node
+    }
+
+    fn index(&self) -> usize {
+        self.index
     }
 
     fn path(&self) -> String {
@@ -47,27 +59,23 @@ impl RenderData {
         "/".to_owned() + &self.path.join("/")
     }
 
-    fn set_current(&mut self, new: &str) {
-        self.current = new.to_string();
-    }
-
     fn push_path(&mut self) {
         self.old_indicies.push(self.index);
         self.index = 0;
-        self.level += 1;
-        self.path.push(self.current.clone());
+        // self.path.push(self.current.clone());
     }
 
     fn pop_path(&mut self) {
         if !self.old_indicies.is_empty() {
             self.index = self.old_indicies.pop().unwrap_or(0);
-            self.level -= 1;
             self.path.pop();
         }
     }
 
     fn inc_index(&mut self) {
-        self.index += 1;
+        if self.index < node_size(self.curr_node) - 1 {
+            self.index += 1;
+        }
     }
 
     fn dec_index(&mut self) {
@@ -81,7 +89,7 @@ fn main() -> Result<()> {
     let file = fs::read_to_string(path).context("File Input")?;
 
     let mut stdout = io::stdout();
-    if let Err(e) = main_loop(&stdout, &file) {
+    if let Err(e) = main_loop(&mut stdout, &file) {
         execute!(
             stdout,
             cursor::Show,
@@ -96,81 +104,22 @@ fn main() -> Result<()> {
 }
 
 #[allow(clippy::too_many_lines)]
-fn main_loop(mut stdout: &io::Stdout, file: &str) -> Result<()> {
+fn main_loop(stdout: &mut io::Stdout, file: &str) -> Result<()> {
+    // let (columns, rows) = terminal::size()?;
     let value: Value = serde_json::from_str(file).context("Json Deserialization")?;
-    // let value = value.as_object().unwrap();
-
-    let mut render_data = RenderData::default();
-    let mut node = &value;
+    let mut render_data = RenderData::new(&value);
 
     execute!(stdout, cursor::Hide, terminal::EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
 
     loop {
-        queue!(
-            stdout,
-            cursor::MoveTo(0, 0),
-            terminal::Clear(terminal::ClearType::All)
-        )?;
-        match node {
-            Value::Array(vec) => {
-                for (i, v) in vec.iter().enumerate() {
-                    if i == render_data.index() {
-                        let s = i.to_string();
-                        render_data.set_current(&s);
-                        queue!(stdout, SetForegroundColor(Color::Blue))?;
-                    }
-                    let str = match v {
-                        Value::Object(map) => {
-                            map.keys().fold("{ ".to_owned(), |acc, k| acc + k) + " }"
-                        }
-                        Value::Array(arr) => {
-                            "[ ".to_owned()
-                                + &arr.first().map_or(String::new(), ToString::to_string)
-                                + " ]"
-                        }
-                        Value::Bool(val) => val.to_string(),
-                        Value::String(val) => val.to_string(),
-                        Value::Number(val) => val.to_string(),
-                        Value::Null => "null".to_string(),
-                    };
-                    queue!(
-                        stdout,
-                        Print(str),
-                        SetForegroundColor(Color::White),
-                        cursor::MoveToNextLine(1),
-                        cursor::MoveToColumn(render_data.indent()),
-                    )?;
-                }
-            }
-            Value::Object(obj) => {
-                for (i, (k, v)) in obj.iter().enumerate() {
-                    if i == render_data.index() {
-                        render_data.set_current(k);
-                        queue!(
-                            stdout,
-                            SetForegroundColor(Color::Blue),
-                            Print(k),
-                            SetForegroundColor(Color::White),
-                        )?;
-                    } else {
-                        stdout.queue(Print(k))?;
-                    }
-                    if !(v.is_array() || v.is_object()) {
-                        queue!(stdout, Print(": "), Print(v))?;
-                    }
-                    queue!(
-                        stdout,
-                        cursor::MoveToNextLine(1),
-                        cursor::MoveToColumn(render_data.indent())
-                    )?;
-                }
-            }
-            Value::Bool(v) => queue!(stdout, Print(v))?,
-            Value::String(v) => queue!(stdout, Print(v))?,
-            Value::Number(v) => queue!(stdout, Print(v))?,
-            Value::Null => queue!(stdout, Print("null"))?,
+        queue!(stdout, MoveTo(0, 0), terminal::Clear(terminal::ClearType::All))?;
+        if let Some(prev) = render_data.prev_node() {
+            render_keys(stdout, prev, 0, 0)?;
         }
+        stdout.queue(cursor::MoveTo(0, 0))?;
+        render_keys(stdout, render_data.curr_node(), render_data.index(), 24)?;
+
         stdout.flush()?;
 
         let event = read()?;
@@ -187,20 +136,20 @@ fn main_loop(mut stdout: &io::Stdout, file: &str) -> Result<()> {
         if event == Event::Key(KeyCode::Char('k').into()) {
             render_data.dec_index();
         }
-        if event == Event::Key(KeyCode::Char('l').into()) {
-            render_data.push_path();
-            let path = render_data.path();
-            node = value
-                .pointer(&path)
-                .context(format!("invalid path: {path}"))?;
-        }
-        if event == Event::Key(KeyCode::Char('h').into()) {
-            render_data.pop_path();
-            let path = render_data.path();
-            node = value
-                .pointer(&path)
-                .context(format!("invalid path: {path}"))?;
-        }
+        // if event == Event::Key(KeyCode::Char('l').into()) {
+            // render_data.push_path();
+            // let path = render_data.path();
+            // node = value
+            //     .pointer(&path)
+            //     .context(format!("invalid path: {path}"))?;
+        // }
+        // if event == Event::Key(KeyCode::Char('h').into()) {
+            // render_data.pop_path();
+            // let path = render_data.path();
+            // node = value
+            //     .pointer(&path)
+            //     .context(format!("invalid path: {path}"))?;
+        // }
     }
 
     execute!(
@@ -210,6 +159,54 @@ fn main_loop(mut stdout: &io::Stdout, file: &str) -> Result<()> {
         terminal::LeaveAlternateScreen
     )?;
     terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+fn render_keys(stdout: &mut io::Stdout, node: &Value, index: usize, column: u16) -> Result<()> {
+    match node {
+        Value::Array(vec) => {
+            for i in 0..vec.len() {
+                if i == index {
+                    queue!(stdout, SetForegroundColor(Color::Blue))?;
+                }
+                queue!(stdout, MoveToColumn(column), Print(i), MoveToNextLine(1))?;
+                queue!(stdout, SetForegroundColor(Color::White))?;
+            }
+        }
+        Value::Object(map) => {
+            for (i, k) in map.keys().enumerate() {
+                if i == index {
+                    queue!(stdout, SetForegroundColor(Color::Blue))?;
+                }
+                queue!(stdout,  cursor::MoveToColumn(column), Print(k), cursor::MoveToNextLine(1))?;
+                queue!(stdout, SetForegroundColor(Color::White))?;
+            }
+        },
+        Value::Bool(v) => queue!(stdout, Print(v))?,
+        Value::String(v) => queue!(stdout, Print(v))?,
+        Value::Number(v) => queue!(stdout, Print(v))?,
+        Value::Null => queue!(stdout, Print("null"))?,
+    }
+    Ok(())
+}
+
+fn render_values(stdout: &mut io::Stdout, node: &Value, column: u16) -> Result<()> {
+    match node {
+        Value::Array(vec) => {
+            for v in vec.iter() {
+                queue!(stdout,  cursor::MoveToColumn(column), Print(v), cursor::MoveToNextLine(1))?;
+            }
+        }
+        Value::Object(map) => {
+            for v in map.values() {
+                queue!(stdout,  cursor::MoveToColumn(column), Print(v), cursor::MoveToNextLine(1))?;
+            }
+        },
+        Value::Bool(v) => queue!(stdout, Print(v))?,
+        Value::String(v) => queue!(stdout, Print(v))?,
+        Value::Number(v) => queue!(stdout, Print(v))?,
+        Value::Null => queue!(stdout, Print("null"))?,
+    }
     Ok(())
 }
 
@@ -223,3 +220,13 @@ fn flush_resize_events(first_resize: (u16, u16)) -> ((u16, u16), (u16, u16)) {
 
     (first_resize, last_resize)
 }
+
+fn node_size(node: &Value) -> usize {
+    match node {
+        Value::Object(map) => map.len(),
+        Value::Array(arr) => arr.len(),
+        Value::Null => 0,
+        _ => 1,
+    }
+}
+
